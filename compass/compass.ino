7084a9633b3cef9a8977ec6f4d2534d65cbb502a
KusumaAonsatit <KusumaAonsatit@users.noreply.github.com>
2018-02-21 21:48:26 +0700

    21-2-61_1

--- compass/compass.ino
+++ compass/compass.ino
@@ -1,38 +1,42 @@
-#include <HMC5983.h>
-#include <Wire.h> //I2C Arduino Library
-#define addr 0x1E //I2C Address for The HMC5883
+//Arduino 1.0+ Only
+//Arduino 1.0+ Only
+
+/*Based largely on code by  Jim Lindblom
+
+ Get pressure, altitude, and temperature from the BMP085.
+ Serial.print it out at 9600 baud to serial monitor.
+ */
+
+#include <Wire.h>
+#include <BMP085.h>
+
+BMP085 pressure_m;
 void setup(){
-Serial.begin(9600);
-Wire.begin();
-Wire.beginTransmission(addr); //start talking
-Wire.write(0x02); // Set the Register
-Wire.write(0x00); // Tell the HMC5883 to Continuously Measure
-Wire.endTransmission();
+  Serial.begin(9600);
+  Wire.begin();
+
+  pressure_m.bmp085Calibration();
 }
+
 void loop()
 {
-int x,y,z; //triple axis data
-//Tell the HMC what regist to begin writing data into
-Wire.beginTransmission(addr);
-Wire.write(0x03); //start with register 3.
-Wire.endTransmission();
-//Read the data.. 2 bytes for each axis.. 6 total bytes
-Wire.requestFrom(addr, 6);
-if(6<=Wire.available()){
-  x = Wire.read()<<8; //MSB  x 
-  x |= Wire.read(); //LSB  x
-  z = Wire.read()<<8; //MSB  z
-  z |= Wire.read(); //LSB z
-  y = Wire.read()<<8; //MSB y
-  y |= Wire.read(); //LSB y
-}
-// Show Values
-Serial.print("X Value: ");
-Serial.println(x);
-Serial.print("Y Value: ");
-Serial.println(y);
-Serial.print("Z Value: ");
-Serial.println(z);
-Serial.println();
-delay(1000);
+  float temperature = pressure_m.bmp085GetTemperature(); //MUST be called first
+  float pressure = pressure_m.bmp085GetPressure();
+  float altitude = pressure_m.calcAltitude(pressure);
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature, 2); //display 2 decimal places
+  Serial.println("deg C");
+
+  Serial.print("Pressure: ");
+  Serial.print(pressure, 0); //whole number only.
+  Serial.println(" Pa");
+
+  Serial.print("Altitude: ");
+  Serial.print(altitude, 2); //display 2 decimal places
+  Serial.println(" M");
+
+  Serial.println();//line break
+
+  delay(1000); //wait a second and get values again.
 }
