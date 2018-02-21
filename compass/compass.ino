680971044d09a6aabbc35355ebf958b48d6f496b
KusumaAonsatit <KusumaAonsatit@users.noreply.github.com>
2018-02-21 22:27:31 +0700

    21-2-61_2

--- compass/compass.ino
+++ compass/compass.ino
@@ -1,42 +1,17 @@
-//Arduino 1.0+ Only
-//Arduino 1.0+ Only
-
-/*Based largely on code by  Jim Lindblom
-
- Get pressure, altitude, and temperature from the BMP085.
- Serial.print it out at 9600 baud to serial monitor.
- */
-
+#include <HMC5983.h>
 #include <Wire.h>
-#include <BMP085.h>
 
-BMP085 pressure_m;
-void setup(){
+HMC5983 compass;
+
+void setup () {
   Serial.begin(9600);
-  Wire.begin();
-
-  pressure_m.bmp085Calibration();
+  compass.begin(); // use "true" if you need some debug information
 }
 
-void loop()
-{
-  float temperature = pressure_m.bmp085GetTemperature(); //MUST be called first
-  float pressure = pressure_m.bmp085GetPressure();
-  float altitude = pressure_m.calcAltitude(pressure);
-
-  Serial.print("Temperature: ");
-  Serial.print(temperature, 2); //display 2 decimal places
-  Serial.println("deg C");
-
-  Serial.print("Pressure: ");
-  Serial.print(pressure, 0); //whole number only.
-  Serial.println(" Pa");
-
-  Serial.print("Altitude: ");
-  Serial.print(altitude, 2); //display 2 decimal places
-  Serial.println(" M");
-
-  Serial.println();//line break
-
-  delay(1000); //wait a second and get values again.
+void loop() {
+  float c ;
+  c = compass.read();
+  
+    Serial.println(c);
+    delay(500);
 }
