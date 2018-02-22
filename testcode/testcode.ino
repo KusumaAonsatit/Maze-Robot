ff058dc73bb610f5632d910b928adc11764e724f
KusumaAonsatit <KusumaAonsatit@users.noreply.github.com>
2018-02-22 22:42:30 +0700

    22-2-61

--- testcode/testcode.ino
+++ testcode/testcode.ino
@@ -8,7 +8,7 @@ NewPing sonarl(9, 8);
 NewPing sonarr(13, 12);
 
 void motorFront(int speed);
-//void motorFront1(int speed);
+void motorFront1(int speed);
 void motorTurnback(int speed);
 void motorTurnright(int speed);
 void motorTurnleft(int speed);
@@ -23,9 +23,11 @@ int pinB1 = 5;
 int pinB2 = 7;
 
 long inches,inchesl,inchesr;
-int c,com;
-HMC5983 compass;
 
+float com;
+float c = -999;
+
+HMC5983 compass;
 
 void setup() {
   Serial.begin(9600);
@@ -48,52 +50,58 @@ void loop() {
   inchesl = sonarl.ping_in();
   inchesr = sonarr.ping_in();
 
-  _compass();
-
 //If the robot detects an obstacle less than four inches away, it will back up, then turn left; if no obstacle is detected, it will go forward
  if(inchesl < 5 && inches > 6 && inchesr < 5 ){ 
   motorFront(200);  
-  }//1
-  else if(inchesl < 4 && inches < 6 && inchesr < 4){  
-     com = c-180; 
-  motorTurnback(220);
- 
-  }//2
-
+  }//Front1
+ else if(inchesl < 6 && inches > 6 && inchesr > 6){  
+  motorFront(255); 
+  }//Front2
+ else if(inchesl > 6 && inches > 6 && inchesr < 6){  
+  motorFront(255); 
+  }//Front3
+ else if(inchesl < 4 && inches < 6 && inchesr < 4){  
+  com = c-180; 
+  motorTurnback(200); 
+  }//Turnback4
+ else { 
+  motorFront(255);  
+  }//other 
 }
 
 void _compass() {
-  float c = -999;
+  
   c = compass.read();
-  //Serial.println(c);
   //delay(500);
 }
 
 void motorFront(int speed){
   
-  // Motor A left
+    // Motor A left
     analogWrite(enableA, speed);
     digitalWrite(pinA1, LOW);
     digitalWrite(pinA2, HIGH);
- // Motor B
+    // Motor B
     analogWrite(enableB, speed); 
     digitalWrite(pinB1, LOW);
     digitalWrite(pinB2, HIGH);
     
     delay(50);
 }
+
 void motorTurnback(int speed){
 
   while(c!=com){
-  // Motor A 
+    // Motor A 
     analogWrite(enableA, speed);
     digitalWrite(pinA1, HIGH);
     digitalWrite(pinA2, LOW);
-  // Motor B
+    // Motor B
     analogWrite(enableB, speed); 
     digitalWrite(pinB1, LOW);
     digitalWrite(pinB2, HIGH);
   }
 
-
 }
+
+
