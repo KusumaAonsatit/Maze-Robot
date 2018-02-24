a2c9d0c0fe765466f718326287804e7464f8cd40
KusumaAonsatit <KusumaAonsatit@users.noreply.github.com>
2018-02-25 00:49:21 +0700

    25-2-61

--- testcode/testcode.ino
+++ testcode/testcode.ino
@@ -23,9 +23,11 @@ int pinB2 = 5;
 
 long inches,inchesl,inchesr;
 
-float com;
+float com=0;
 float c = -999;
 
+
+char ch;
 HMC5983 compass;
 
 void setup() {
@@ -42,25 +44,64 @@ void setup() {
 }
 
 void loop() {
+   _compass();
+   ch = Serial.read();
+   if(ch == 'F'){
+      motorFront(100);
+      delay(1000);
+   }
+   if(ch == 'L'){
+      //while(c!=com){
+          motorLeft(80);
+           delay(1000);
+      //}
+   }
+   if(ch == 'l'){
+      motorLeft_Smoot(70);
+       delay(1000);
+   }
+   if(ch =='R'){
+      motorRight(80);
+       delay(1000);
+   }
+   if(ch == 'r'){
+      motorRight_Smoot(70);
+       delay(1000);
+   }
+   if(ch == 'S'){
+      motorSTOP();
+   }
+   if(ch == 'B'){
+       if(c<165){
+           com = 165 - c;
+           com = 360 - com;
+       }
+       else
+          com = c - 165;
+       //com = c - 180.00;
+       motorTurnBack(80);
+   }
 
 
+
+   
 //Ping the sensor and determine the distance in inches
   inches = sonar.ping_in();  // front
   inchesl = sonarl.ping_in(); // left
   inchesr = sonarr.ping_in(); // Right
   
-  _compass();
+  
   Serial.print("        Left = ");
   Serial.print(inchesl);
   Serial.print("    Front = ");
   Serial.print(inches);
   Serial.print("    Right = ");
   Serial.println(inchesr);
-  
+  /*
 //If the robot detects an obstacle less than four inches away, it will back up, then turn left; if no obstacle is detected, it will go forward
  
  if(inchesl < 5 && inches > 6 && inchesr < 5 ){ //Front1
-      motorFront(100);  
+      motorFront(100);
   }
   
  else if(inchesl < 6 && inches > 6 && inchesr > 6){  //Front2
@@ -75,6 +116,8 @@ void loop() {
  else { //other 
   motorFront(100);  
   }
+
+  */
 }
 
 void _compass() {
@@ -85,8 +128,36 @@ void _compass() {
   //delay(500);
 }
 
+
+
+void motorTurnBack(int speed){
+  Serial.print("COM = ");
+  Serial.println(com);
+  while(1){
+      _compass();
+      Serial.print("C = ");
+      Serial.print(c);
+      Serial.print("  COM = ");
+      Serial.println(com);
+      motorLeft(speed);
+      
+      if(c > (com-2.5) && c < (com+2.5)){
+          break;
+      }
+  }
+  Serial.println("///////////////////////////STOP/////////////////////////////");
+  motorSTOP();
+  delay(500000);
+  }
+
+
+
+
+
+////////////////////Standard Function///////////////////////
+
 void motorFront(int speed){
-  
+
     // Motor A left
     analogWrite(enableA, speed);
     digitalWrite(pinA1, LOW);
@@ -99,21 +170,60 @@ void motorFront(int speed){
     //delay(50);
 }
 
-void motorTurnback(int speed){
 
-  while(c!=com){
-     // Motor A left
+void motorLeft_Smoot(int speed){
+    // Motor A left
     analogWrite(enableA, speed);
+    digitalWrite(pinA1, LOW);
+    digitalWrite(pinA2, HIGH);
+    // Motor B Right
+    analogWrite(enableB, speed + 20 + 15); 
+    digitalWrite(pinB1, LOW);
+    digitalWrite(pinB2, HIGH);
+}
+void motorLeft(int speed){
+   analogWrite(enableA, speed);
     digitalWrite(pinA1, HIGH);
     digitalWrite(pinA2, LOW);
     // Motor B Right
-    analogWrite(enableB, speed); 
+    analogWrite(enableB, speed + 20); 
     digitalWrite(pinB1, LOW);
     digitalWrite(pinB2, HIGH);
-  }
-  motorFront(100);
-  delay(1000);
-
 }
+
+
+
+void motorRight_Smoot(int speed){
+    // Motor A left
+    analogWrite(enableA, speed + 20);
+    digitalWrite(pinA1, LOW);
+    digitalWrite(pinA2, HIGH);
+    // Motor B Right
+    analogWrite(enableB, speed + 20 ); 
+    digitalWrite(pinB1, LOW);
+    digitalWrite(pinB2, HIGH);
+}
+void motorRight(int speed){
+   // Motor A left
+    analogWrite(enableA, speed);
+    digitalWrite(pinA1, LOW);
+    digitalWrite(pinA2, HIGH);
+    // Motor B Right
+    analogWrite(enableB, speed + 20 ); 
+    digitalWrite(pinB1, HIGH);
+    digitalWrite(pinB2, LOW);
+}
+
+void motorSTOP(){
+   analogWrite(enableA,0);
+    digitalWrite(pinA1, LOW);
+    digitalWrite(pinA2, HIGH);
+    // Motor B Right
+    analogWrite(enableB,0); 
+    digitalWrite(pinB1, HIGH);
+    digitalWrite(pinB2, LOW);
+}
+////////////////END  Standard  Function ///////////////////////
+
 
 
