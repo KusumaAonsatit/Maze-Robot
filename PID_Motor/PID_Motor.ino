1c01ef38f073af090278cbef6fff7544387e17d8
KusumaAonsatit <KusumaAonsatit@users.noreply.github.com>
2018-02-28 12:14:25 +0700

    28-2-61

---
+++ PID_Motor/PID_Motor.ino
@@ -0,0 +1,109 @@
+  // Motor A Left
+    int dir1PinA = 2;
+    int dir2PinA = 3;
+    int speedPinA = 6; //   เพื่อให้ PWM สามารถควบคุมความเร็วมอเตอร์ 
+// Motor B Right
+    int dir1PinB = 4;
+    int dir2PinB = 5;
+    int speedPinB = 7; // เพื่อให้ PWM สามารถควบคุมความเร็วมอเตอร์
+//Counter speed sensor-----------------------------------------------------------------------//
+    const byte interruptPinL = 18;
+    const byte interruptPinR = 19;
+    
+    long Left,Right;
+    int normalSpeed = 192; // 25% Duty Cycle
+  class PID{
+public:
+  double error;
+  double sample;
+  double lastSample;
+  double kP, kI, kD;      
+  double P, I, D;
+  double pid;
+  
+  double setPoint;
+  long lastProcess; 
+  PID(double _kP, double _kI, double _kD){
+    kP = _kP;
+    kI = _kI;
+    kD = _kD;
+  }
+  void addNewSample(double _sample){
+    sample = _sample;
+  } 
+  void setSetPoint(double _setPoint){
+    setPoint = _setPoint;
+  } 
+  double process(){
+    // Implementação P ID
+    error = setPoint - sample;
+    float deltaTime = (millis() - lastProcess) / 1000.0;
+    lastProcess = millis();
+    //P
+    P = error * kP;
+    //I
+    I = I + (error * kI) * deltaTime;
+    
+    //D
+    D = (lastSample - sample) * kD / deltaTime;
+    lastSample = sample;
+    // Soma tudo
+    pid = P + I + D;   
+    return pid;
+  }
+};
+PID pid(3, 7, 0.05);
+void setup() {
+  pid.setSetPoint(0.0);
+   Serial.begin(9600);//set board
+//interrupt--------------------------------------------------------------------
+  pinMode(interruptPinL, INPUT);
+  pinMode(interruptPinR, INPUT);
+  attachInterrupt(digitalPinToInterrupt(18), ISR_L, RISING );
+  attachInterrupt(digitalPinToInterrupt(19), ISR_R, RISING  );
+//Motor--------------------------------------------------------------------------
+  pinMode(dir1PinA,OUTPUT);
+  pinMode(dir2PinA,OUTPUT);
+  pinMode(speedPinA,OUTPUT);
+  pinMode(dir1PinB,OUTPUT);
+  pinMode(dir2PinB,OUTPUT);
+  pinMode(speedPinB,OUTPUT);
+}
+void ISR_L(){
+    Left++;
+}
+void ISR_R(){
+    Right++; }
+void loop() {
+  // TODO: critical section
+  long error = Left - Right;
+  long minCount = min(Left, Right);
+  Left -= minCount;
+  Right -= minCount;
+  // Add new sample to PID
+  pid.addNewSample(error);
+  // PID feedback
+  int powerDiff = int(pid.process());
+  Serial.print("Error: ");
+  Serial.print(error);
+  Serial.print(" | powerDiff: ");
+  Serial.println(powerDiff);
+  // Update motor speed
+  if (powerDiff < 0)
+    set_power(normalSpeed + powerDiff, normalSpeed);
+  else
+    set_power(normalSpeed, normalSpeed - powerDiff);
+}
+int set_power(int speed_left,int speed_right){
+  speed_left = max(min(speed_left, 255), 0);
+  speed_right = max(min(speed_right, 255), 0);
+  // Motor A ด้านซ้าย
+  analogWrite(speedPinA, speed_left); //ตั้งค่าความเร็ว PWM ผ่านตัวแปร ค่าต่ำลง มอเตอร์จะหมุนช้าลง
+  digitalWrite(dir1PinA, LOW);
+  digitalWrite(dir2PinA, HIGH);
+  // Motor B ด้านขวา
+  analogWrite(speedPinB, speed_right); //ตั้งค่าความเร็ว PWM ผ่านตัวแปร ค่าต่ำลง มอเตอร์จะหมุนช้าลง
+  digitalWrite(dir1PinB, LOW);
+  digitalWrite(dir2PinB, HIGH);
+}
+
--- testcode/testcode.ino
+++ testcode/testcode.ino
@@ -8,32 +8,22 @@ NewPing sonarl(9, 8);
 NewPing sonarr(13, 12);
 
 void motorFront(int speed);
+void motorFront1(int speed);
 void motorTurnback(int speed);
 void motorTurnright(int speed);
 void motorTurnleft(int speed);
 
-// calibreat //
-//float v0 = 0;
-float v90 = 82;
-float v180 = 180;
-float v270 = 278;
-/////////////////////////////////
 
 int enableA = 6;
 int pinA1 = 2;
 int pinA2 = 3;
 
-int enableB = 7;
-int pinB1 = 4;
-int pinB2 = 5;
+int enableB = 4;
+int pinB1 = 5;
+int pinB2 = 7;
 
 long inches,inchesl,inchesr;
 
-float com=0;
-float c = -999;
-
-
-char ch;
 HMC5983 compass;
 
 void setup() {
@@ -50,238 +40,101 @@ void setup() {
 }
 
 void loop() {
-   _compass();
-  inches = sonar.ping_in();  // front
-  inchesl = sonarl.ping_in(); // left
-  inchesr = sonarr.ping_in(); // Right
-  
-  
-  Serial.print("        Left = ");
-  Serial.print(inchesl);
-  Serial.print("    Front = ");
-  Serial.print(inches);
-  Serial.print("    Right = ");
-  Serial.println(inchesr);
 
 
-
- 
-  
-   
-   ch = Serial.read();
-   if(ch == 'F'){
-      motorFront(70);
-      delay(1000);
-   }
-   if(ch == 'L'){
-      //while(c!=com){
-          motorLeft(80);
-           delay(1000);
-           ch = 'S';
-      //}
-   }
-   if(ch == 'l'){
-      motorLeft_Smoot(70);
-       delay(1000);
-   }
-   if(ch =='R'){
-      motorRight(80);
-       delay(1000);
-       ch = 'S';
-   }
-   if(ch == 'r'){
-      motorRight_Smoot(70);
-       delay(1000);
-   }
-   if(ch == 'S'){
-      motorSTOP();
-   }
-   if(ch == 'G'){
-      motorBack(70);
-   }
-   if(ch == 'B'){
-       if(c<77){
-           com = 77 - c;
-           com = 360 - com;
-       }
-       else
-          com = c - 77;
-       //com = c - 180.00;
-       motorTurnBack(80);
-   }
-   if(ch == 'b'){
-       if(c<180){
-           com = 180 - c;
-           com = 360 - com;
-       }
-       else
-          com = c - 180;
-       //com = c - 180.00;
-       motorTurnBack(61');
-   }
-
-    
-
-   
 //Ping the sensor and determine the distance in inches
-  
-  /*
-//If the robot detects an obstacle less than four inches away, it will back up, then turn left; if no obstacle is detected, it will go forward
- 
- if(inchesl < 5 && inches > 6 && inchesr < 5 ){ //Front1
-      motorFront(100);
-  }
-  
- else if(inchesl < 6 && inches > 6 && inchesr > 6){  //Front2
-      motorFront(100); 
-  }
-  
- else if((inchesl < 4) && (inches < 6) && (inchesr < 4)){  //Turnback
-     com = c-180; 
-     motorTurnback(100); 
-  }
-  
- else { //other 
-  motorFront(100);  
-  }
+  inches = sonar.ping_in();
+  inchesl = sonarl.ping_in();
+  inchesr = sonarr.ping_in();
 
-  */
+//If the robot detects an obstacle less than four inches away, it will back up, then turn left; if no obstacle is detected, it will go forward
+ if(inchesl < 5 && inches > 6 && inchesr < 5 ){ 
+  motorFront(200);  
+  }//1
+ else if(inchesl < 6 && inches > 6 && inchesr > 6){  
+  motorFront(255); 
+  }//2
+ else if(inchesl > 6 && inches > 6 && inchesr < 6){  
+  motorFront(255); 
+  }//3
+ else if(inchesl < 4 && inches < 6 && inchesr < 4){  
+  com = c-180; 
+  motorTurnback(200); 
+  }//4
+ else if(inchesl > 6 && inches < 10 && inchesr < 6){ 
+  com = c-90; 
+  motorTurnleft(240); 
+  }//5
+ else if(inchesl < 6 && inches < 8 && inchesr > 6){ 
+  com = c-90;  
+  motorTurnright(240);
+  }//6
+ else { 
+  motorFront(255);  
+  }//other 
 }
 
 void _compass() {
-  
+  float c = -999;
   c = compass.read();
-  
-  Serial.print("compass = ");
-  Serial.print(c);
-  //delay(500);
+  delay(500);
 }
 
-
-
-void motorTurnBack(int speed){
-  Serial.print("COM = ");
-  Serial.println(com);
-  while(1){
-      _compass();
-      Serial.print("C = ");
-      Serial.print(c);
-      Serial.print("  COM = ");
-      Serial.println(com);
-      motorRight(speed);
-      
-      if(c > (com-2.5) && c < (com+2.5)){
-          break;
-      }
-  }
-  Serial.println("///////////////////////////STOP/////////////////////////////");
-  motorSTOP();
-  delay(500000);
-  }
-
-
-
-
-
-////////////////////Standard Function///////////////////////
-void motorBack(int speed){
-      // Motor A left
-    analogWrite(enableA, speed);
-    digitalWrite(pinA1, HIGH);
-    digitalWrite(pinA2, LOW);
-    // Motor B Right
-    analogWrite(enableB, speed + 20); 
-    digitalWrite(pinB1, HIGH);
-    digitalWrite(pinB2, LOW);
-    
-}
 void motorFront(int speed){
-
+  
     // Motor A left
     analogWrite(enableA, speed);
     digitalWrite(pinA1, LOW);
     digitalWrite(pinA2, HIGH);
-    // Motor B Right
-    analogWrite(enableB, speed + 15); 
+    // Motor B
+    analogWrite(enableB, speed); 
     digitalWrite(pinB1, LOW);
     digitalWrite(pinB2, HIGH);
     
-    //delay(50);
+    delay(50);
 }
 
+void motorTurnback(int speed){
 
-void motorLeft_Smoot(int speed){
-    // Motor A left
+  while(c!=com){
+    // Motor A 
     analogWrite(enableA, speed);
-    digitalWrite(pinA1, LOW);
-    digitalWrite(pinA2, HIGH);
-    // Motor B Right
-    analogWrite(enableB, speed + 20 + 10); 
-    digitalWrite(pinB1, LOW);
-    digitalWrite(pinB2, HIGH);
-}
-void motorLeft(int speed){
-   analogWrite(enableA, speed);
     digitalWrite(pinA1, HIGH);
     digitalWrite(pinA2, LOW);
-    // Motor B Right
-    analogWrite(enableB, speed + 22); 
+    // Motor B
+    analogWrite(enableB, speed); 
     digitalWrite(pinB1, LOW);
     digitalWrite(pinB2, HIGH);
+  }
+
 }
+void motorTurnleft(int speed){
 
-
-
-void motorRight_Smoot(int speed){
-    // Motor A left
-    analogWrite(enableA, speed + 10);
-    digitalWrite(pinA1, LOW);
-    digitalWrite(pinA2, HIGH);
-    // Motor B Right
-    analogWrite(enableB, speed + 20 ); 
+  while(c!=com){
+    // Motor A 
+    analogWrite(enableA, 0);
+    digitalWrite(pinA1, HIGH);
+    digitalWrite(pinA2, LOW);
+    // Motor B
+    analogWrite(enableB, speed); 
     digitalWrite(pinB1, LOW);
     digitalWrite(pinB2, HIGH);
+  }
+
 }
-void motorRight(int speed){
-   // Motor A left
+
+void motorTurnright(int speed){
+
+  while(c!=com){
+    // Motor A
     analogWrite(enableA, speed);
     digitalWrite(pinA1, LOW);
-    digitalWrite(pinA2, HIGH);
-    // Motor B Right
-    analogWrite(enableB, speed + 20 ); 
+    digitalWrite(pinA2,HIGH );
+    // Motor B          
+    analogWrite(enableB, 0); 
     digitalWrite(pinB1, HIGH);
-    digitalWrite(pinB2, LOW);
+    digitalWrite(pinB2, LOW); 
+  }
+
 }
-
-void motorSTOP(){
-   analogWrite(enableA,0);
-    digitalWrite(pinA1, LOW);
-    digitalWrite(pinA2, HIGH);
-    // Motor B Right
-    analogWrite(enableB,0); 
-    digitalWrite(pinB1, HIGH);
-    digitalWrite(pinB2, LOW);
-}
-////////////////END  Standard  Function ///////////////////////
- float CLB(float c){
-      float ans=0;
-      if( c >= 0 && c <= v90)
-          ans = (c * 90)/(v90 - 0);
-          
-      else if( c >= v90 && c <= v180)
-          ans = 90 + (c * 90)/(v180 - v90);
-          
-      else if( c >= v180 && c <= v270)
-          ans = 180 + (c * 90)/(v270 - v180);
-          
-      else if( c >= v270 && c <= 360 )
-          ans = 270 + (c * 90)/(360 - v270);
-
-
-      return ans;
-     
-
-     
-}
-
 
