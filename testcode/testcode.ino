732fd1ce34456bc1300fcbaa16d20a7a606a8f19
KusumaAonsatit <KusumaAonsatit@users.noreply.github.com>
2018-02-28 18:17:42 +0700

    28-2-61_1

--- testcode/testcode.ino
+++ testcode/testcode.ino
@@ -22,6 +22,9 @@ int enableB = 4;
 int pinB1 = 5;
 int pinB2 = 7;
 
+float c;
+float com;
+
 long inches,inchesl,inchesr;
 
 HMC5983 compass;
@@ -47,53 +50,83 @@ void loop() {
   inchesl = sonarl.ping_in();
   inchesr = sonarr.ping_in();
 
-//If the robot detects an obstacle less than four inches away, it will back up, then turn left; if no obstacle is detected, it will go forward
- if(inchesl < 5 && inches > 6 && inchesr < 5 ){ 
-  motorFront(200);  
+  
+ //prin();
+ _compass();
+  
+ if((inchesl < 4) && (inches > 6 )&& (inchesr < 4) ){ 
+  motorFront(100);  
   }//1
- else if(inchesl < 6 && inches > 6 && inchesr > 6){  
-  motorFront(255); 
-  }//2
- else if(inchesl > 6 && inches > 6 && inchesr < 6){  
-  motorFront(255); 
-  }//3
- else if(inchesl < 4 && inches < 6 && inchesr < 4){  
-  com = c-180; 
-  motorTurnback(200); 
-  }//4
- else if(inchesl > 6 && inches < 10 && inchesr < 6){ 
-  com = c-90; 
-  motorTurnleft(240); 
+// else if(inchesl < 4 && inches > 6 && inchesr > 6){  
+//  motorFront(100); 
+//  }//2
+// else if(inchesl > 6 && inches > 6 && inchesr < 4){  
+//  motorFront(100); 
+//  }//3
+// else if(inchesl < 4 && inches < 6 && inchesr < 4){  
+//  com = c-180; 
+//  motorTurnback(100); 
+//  }//4
+ else if(inchesl > 6 && inches < 6 && inchesr < 4){ 
+   if((c > 270) && (c < 360)){
+    com = (c + 30) - 360;
+   }
+   else if ((c > 0) && (c > 90)){
+    com = 90 - c;
+    com = 360 - c;
+   }
+   else{
+    com = c - 90;
+   }
+  motorTurnleft(75); 
   }//5
- else if(inchesl < 6 && inches < 8 && inchesr > 6){ 
-  com = c-90;  
-  motorTurnright(240);
-  }//6
+// else if(inchesl < 4 && inches < 6 && inchesr > 6){ 
+//   if((c > 270) && (c < 360)){
+ 
+//  motorTurnright(100);
+//  }//6
  else { 
-  motorFront(255);  
+  motorFront(100);  
   }//other 
+
 }
 
 void _compass() {
-  float c = -999;
+ // float c = -999;
   c = compass.read();
-  delay(500);
+ // delay(500);
 }
-
+//void prin(){
+//  Serial.print("Left : ");
+//  Serial.print(inchesl);
+//  Serial.print("   Front : ");
+//  Serial.print(inches);
+//  Serial.print("   Right : ");
+//  Serial.println(inchesr);
+//  Serial.print("   compass : ");
+//  Serial.println(c);
+//  delay(100);
+//}
 void motorFront(int speed){
   
-    // Motor A left
-    analogWrite(enableA, speed);
-    digitalWrite(pinA1, LOW);
-    digitalWrite(pinA2, HIGH);
-    // Motor B
-    analogWrite(enableB, speed); 
-    digitalWrite(pinB1, LOW);
-    digitalWrite(pinB2, HIGH);
-    
-    delay(50);
-}
+  // Motor A left
+  
+          analogWrite(enableA, speed-3);
 
+          digitalWrite(pinA1, LOW);
+
+          digitalWrite(pinA2, HIGH);
+
+ // Motor B
+
+    analogWrite(enableB, speed+20); 
+
+    digitalWrite(pinB1, LOW);
+
+    digitalWrite(pinB2, HIGH);
+  
+    //delay(300);
+}
 void motorTurnback(int speed){
 
   while(c!=com){
@@ -109,18 +142,27 @@ void motorTurnback(int speed){
 
 }
 void motorTurnleft(int speed){
-
-  while(c!=com){
     // Motor A 
-    analogWrite(enableA, 0);
+    analogWrite(enableA, speed);
     digitalWrite(pinA1, HIGH);
     digitalWrite(pinA2, LOW);
     // Motor B
     analogWrite(enableB, speed); 
     digitalWrite(pinB1, LOW);
     digitalWrite(pinB2, HIGH);
-  }
+    
+  while(1){
+    _compass();
+    if(c >= com){//เอาแค่ค่าน้อยกว่าหรือมากกว่าอันเดียวพอ 
+     break;
+    }
 
+    Serial.print("heading: ");
+    Serial.print(c);
+    Serial.print(" target: ");
+    Serial.println(com);
+  }
+  motorFront(100);
 }
 
 void motorTurnright(int speed){
@@ -131,10 +173,32 @@ void motorTurnright(int speed){
     digitalWrite(pinA1, LOW);
     digitalWrite(pinA2,HIGH );
     // Motor B          
-    analogWrite(enableB, 0); 
+    analogWrite(enableB, speed); 
     digitalWrite(pinB1, HIGH);
     digitalWrite(pinB2, LOW); 
   }
 
 }
-
+/*********************************************************************************
+ if((c > 270) && (c < 360)){
+  com = (c + 30) - 360;
+ }
+ else if ((c > 0) && (c > 90)){
+  com = 90 - x;
+  x = 360 - x;
+ }
+ else{
+  com = c - 90;
+ }
+/*********************************************************************************
+  if((c > 270) && (c < 360)){
+  com = (c + 30) - 360;
+ }
+ else if ((c > 0) && (c > 90)){
+  com = 90 - x;
+  x = 360 - x;
+ }
+ else{
+  com = c + 90;
+ }
+ */
