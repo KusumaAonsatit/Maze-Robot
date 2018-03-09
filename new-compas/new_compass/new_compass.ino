61f600614f4a7a219fa7f7c968e2345849d1a9ad
KusumaAonsatit <KusumaAonsatit@users.noreply.github.com>
2018-03-09 22:48:03 +0700

    9-3-61

--- new-compas/new_compass/new_compass.ino
+++ new-compas/new_compass/new_compass.ino
@@ -16,25 +16,32 @@ void readCompass_ISR() {
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(250000); // this is going to be hot... :)
+  Serial.begin(9600); // this is going to be hot... :)
+
+  Serial.println("Booting ...");
 
   while (!compass.begin(readCompass_ISR, 0)) {
     Serial.println("HMC5983 Problem");
     delay(500);
-  }  
+  }
+
+  Serial.println("Ready!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (compass_rdy) {
+  // if (compass_rdy) {
+  {
     compass_rdy = false;
     
     double heading = compass.read(); // heading in 0-360º format
     read_time = (micros() - last_read_time) / 1000;
     last_read_time = micros();
-    if (fmod(counter, 1.5) == 0) {
-      Serial.println("heading=" + String(heading, DEC) + ", ms=" + String(read_time, DEC)); 
-    }
+    
+    Serial.println("heading=" + String(heading, DEC) + ", ms=" + String(read_time, DEC)); 
+    
     counter++;
+
+    delay(200);
   }
 }
